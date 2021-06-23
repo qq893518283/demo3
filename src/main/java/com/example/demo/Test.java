89e5a473c468f69643a0d8e97d486d46c18b0758
admin <admin>
2021-06-23 11:03:26 +0800

    master__1.1

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @Author sxk
+ * @Date 2021/6/23 11:02
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("master");
+	}
+}
